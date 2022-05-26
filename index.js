1731a6324652a1ca25e0a7468ad205536a561c62
William Berbet <wiiliamb@hotmail.com>
2022-05-26 12:55:17 -0300

    mongoose install

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 
+const data = require("./data")
+
 const app = express();
 
 
 app.use(express.json());
 
 
-app.use("/", (req, res) => {
-    return res.status(200).json("Aoba?")
+//app.use("/", (req, res) => {
+  //  return res.status(200).json("Aoba?")
     
-})
+//})
 
-
+app.use("/rotaParaData", (req, res) => {
+    data.push(req.body)
+        
+    return res.status(201).json([...data])
+});
 
 app.listen(4000, () => {
     console.log("Server tá dibas!")
