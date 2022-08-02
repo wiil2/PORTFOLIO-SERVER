868603ae1dac1ea0ca2df584a3a25c472c5487bc
William Berbet <wiiliamb@hotmail.com>
2022-08-02 16:02:29 -0300

    update project model

--- models/Projects.model.js
+++ models/Projects.model.js
@@ -5,7 +5,9 @@ const projectsSchema = new Schema({
     language: {type: String, required: true },
     description: {type: String, required: true, min: 10, max: 150},
     tags: {type: String, required: true, enum:["FINALIZADO", "EM ANDAMENTO"]},
-    img: { type: String, default: "https://mustlovelists.com/wp-content/uploads/2021/12/Project-notebook.jpg" }
+    img: { type: String, default: "https://mustlovelists.com/wp-content/uploads/2021/12/Project-notebook.jpg" },
+    repo: { type: String },
+    project : { type: String }
 });
 
 const ProjectsModel = model("Projects", projectsSchema );
